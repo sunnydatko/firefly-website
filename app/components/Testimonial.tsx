5fb67356ffea6583e3605eab978c62db4692da3e
Sunny Datko <sunnydatko@Mac.attlocal.net>
2026-06-07 13:04:22 -0700

    fix testimonial overflow

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -25,6 +25,7 @@ export default function Testimonial() {
         borderTop: "1px solid rgba(255,255,255,0.06)",
         position: "relative",
         zIndex: 1,
+        overflow: "hidden",
       }}
     >
       <Box
